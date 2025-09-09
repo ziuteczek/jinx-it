32c16005d14dea57ee43f9f1180d1558f474ae7b
ziuteczek <135015871+ziuteczek@users.noreply.github.com>
2025-09-09 13:21:45 +0000

    refactor, dummy textures added, resolution changed

---
+++ build/CMakeFiles/jinx.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/jinx-it/src/engine.c" "CMakeFiles/jinx.dir/src/engine.c.o" "gcc" "CMakeFiles/jinx.dir/src/engine.c.o.d"
+  "/workspaces/jinx-it/src/main.c" "CMakeFiles/jinx.dir/src/main.c.o" "gcc" "CMakeFiles/jinx.dir/src/main.c.o.d"
+  "/workspaces/jinx-it/src/render.c" "CMakeFiles/jinx.dir/src/render.c.o" "gcc" "CMakeFiles/jinx.dir/src/render.c.o.d"
+  "/workspaces/jinx-it/src/update.c" "CMakeFiles/jinx.dir/src/update.c.o" "gcc" "CMakeFiles/jinx.dir/src/update.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/jinx.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jinx.dir/src/engine.c.o"
+  "CMakeFiles/jinx.dir/src/engine.c.o.d"
+  "CMakeFiles/jinx.dir/src/main.c.o"
+  "CMakeFiles/jinx.dir/src/main.c.o.d"
+  "CMakeFiles/jinx.dir/src/render.c.o"
+  "CMakeFiles/jinx.dir/src/render.c.o.d"
+  "CMakeFiles/jinx.dir/src/update.c.o"
+  "CMakeFiles/jinx.dir/src/update.c.o.d"
+  "jinx"
+  "jinx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jinx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
